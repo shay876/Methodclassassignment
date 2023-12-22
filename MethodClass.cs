43aec5043f2307e71f140502dfccf022d0d70ef5
shay876 <111309646+shay876@users.noreply.github.com>
2023-12-21 18:45:29 -0700

    Add project files.

---
+++ MethodClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Create a class. In that class, create a void method that takes
+//two integers as parameters. Have the method do a math operation on
+//the first integer and display the second integer to the screen. 
+namespace Methodclassassignment
+{
+    class MethodClass
+    {
+        public void calculateArea(int width, int height)
+        {
+            int result = width * height;
+            Console.WriteLine("The area is " + result);
+            Console.WriteLine("The width is " + width + " and the height is " + height);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+
+namespace Methodclassassignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //In the Main() method of the console app, instantiate the class
+            MethodClass newObject = new MethodClass();
+
+            //Call the method in the class, passing in two numbers.
+            Console.WriteLine("Enter a number");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter another number");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+            //Call the method in the class, specifying the parameters
+            //by name.
+            newObject.calculateArea(numOne, numTwo);
+
+        }
+
+
+    }
+
+}
